e2392b56f4db48e02ad2f2b8fd10440d1398de6e
Ryan MacInnes <ryan.macinnes@gmail.com>
2025-10-24 14:06:11 +0900

    Doing stylesheets in document, not head component.

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,6 @@ export default function Home() {
         <meta name='description' content='Good Dog Coven - A mystical retreat for your goodest dog' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
-
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin />
-        <link href='https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap' rel='stylesheet' />
       </Head>
 
       <header>
